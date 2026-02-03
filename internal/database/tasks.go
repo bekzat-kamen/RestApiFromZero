18c99b72d280fe6223fccd38bfc3838d0bcd3c09
Bekzat Kamen <begzat.kamen@gmail.com>
2026-02-03 18:06:52 +0500

    End Project

--- internal/database/tasks.go
+++ internal/database/tasks.go
@@ -22,78 +22,160 @@ func NewTaskStore(db *sqlx.DB) *TaskStore {
 func (s *TaskStore) GetAll() ([]models.Task, error) {
 	var tasks []models.Task
 
-	query := `SELECT * FROM tasks order by created_at desc`
-	err := s.db.Select(&tasks, query)
-	if err != nil {
+	query := `
+		SELECT
+			id,
+			title,
+			description,
+			completed,
+			created_at,
+			updated_at
+		FROM tasks
+		ORDER BY created_at DESC
+	`
+
+	if err := s.db.Select(&tasks, query); err != nil {
 		return nil, err
 	}
+
 	return tasks, nil
 }
 
 func (s *TaskStore) GetByID(id int) (*models.Task, error) {
 	var task models.Task
-	query := `SELECT * FROM tasks WHERE id = $1`
+
+	query := `
+		SELECT
+			id,
+			title,
+			description,
+			completed,
+			created_at,
+			updated_at
+		FROM tasks
+		WHERE id = $1
+	`
+
 	err := s.db.Get(&task, query, id)
-
-	if err == sql.ErrNoRows {
-		return nil, fmt.Errorf("task not found with id %d", id)
-	}
-
 	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("task not found with id %d", id)
+		}
 		return nil, err
 	}
+
 	return &task, nil
 }
-
 func (s *TaskStore) Create(input models.CreateTaskInput) (*models.Task, error) {
 	var task models.Task
-	query := `INSERT INTO tasks (title, description, completed, created_at, updated_at) VALUES ($1, $2) returning id,title,description,completed,created_at,updated_at`
 	now := time.Now()
-	err := s.db.QueryRowx(query, input.Title, input.Description, input.Completed, now, now).StructScan(&task)
+
+	query := `
+		INSERT INTO tasks (
+			title,
+			description,
+			completed,
+			created_at,
+			updated_at
+		)
+		VALUES ($1, $2, $3, $4, $5)
+		RETURNING
+			id,
+			title,
+			description,
+			completed,
+			created_at,
+			updated_at
+	`
+
+	err := s.db.
+		QueryRowx(
+			query,
+			input.Title,
+			input.Description,
+			input.Completed,
+			now,
+			now,
+		).
+		StructScan(&task)
+
 	if err != nil {
 		return nil, err
 	}
+
 	return &task, nil
 }
 
 func (s *TaskStore) Update(id int, input models.UpdateTaskInput) (*models.Task, error) {
-	task, err := s.GetByID(id)
+	existingTask, err := s.GetByID(id)
 	if err != nil {
 		return nil, err
 	}
 
 	if input.Title != nil {
-		task.Title = *input.Title
+		existingTask.Title = *input.Title
 	}
 	if input.Description != nil {
-		task.Description = *input.Description
+		existingTask.Description = *input.Description
 	}
 	if input.Completed != nil {
-		task.Completed = *input.Completed
+		existingTask.Completed = *input.Completed
 	}
-	task.UpdatedAt = time.Now()
 
-	query := `UPDATE tasks SET title=$1, description=$2, completed=$3, updated_at=$4 WHERE id = $5 returning id,title,description,completed,created_at,updated_at`
+	existingTask.UpdatedAt = time.Now()
+
+	query := `
+		UPDATE tasks
+		SET
+			title = $1,
+			description = $2,
+			completed = $3,
+			updated_at = $4
+		WHERE id = $5
+		RETURNING
+			id,
+			title,
+			description,
+			completed,
+			created_at,
+			updated_at
+	`
+
 	var updatedTask models.Task
-	err = s.db.QueryRowx(query, task.Title, task.Description, task.Completed, task.UpdatedAt, id).StructScan(&updatedTask)
+	err = s.db.
+		QueryRowx(
+			query,
+			existingTask.Title,
+			existingTask.Description,
+			existingTask.Completed,
+			existingTask.UpdatedAt,
+			id,
+		).
+		StructScan(&updatedTask)
+
 	if err != nil {
 		return nil, err
 	}
-	return task, nil
+
+	return &updatedTask, nil
 }
 
 func (s *TaskStore) Delete(id int) error {
 	query := `DELETE FROM tasks WHERE id = $1`
+
 	result, err := s.db.Exec(query, id)
 	if err != nil {
 		return err
 	}
-	rows, err := result.RowsAffected()
+
+	rowsAffected, err := result.RowsAffected()
 	if err != nil {
 		return err
 	}
-	if rows == 0 {
+
+	if rowsAffected == 0 {
 		return fmt.Errorf("task not found with id %d", id)
 	}
+
 	return nil
 }
--- internal/models/task.go
+++ internal/models/task.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Task struct {
-	ID          int       `json:"id" db: "id"`
-	Title       string    `json:"title" db: "title"`
-	Description string    `json:"description" db: "description"`
-	Completed   bool      `json:"completed" db: "completed"`
-	CreatedAt   time.Time `json:"createdAt" db: "createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt" db: "updatedAt"`
+	ID          int       `json:"id" db:"id"`
+	Title       string    `json:"title" db:"title"`
+	Description string    `json:"description" db:"description"`
+	Completed   bool      `json:"completed" db:"completed"`
+	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt   time.Time `json:"updatedAt" db:"updated_at"`
 }
 
 type CreateTaskInput struct {
